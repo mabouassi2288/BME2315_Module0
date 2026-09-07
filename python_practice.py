335eaca01e562e1368cfe0744ee8849516b2b791
mabouassi2288 <xnx4pa@virginia.edu>
2026-09-07 10:01:34 -0400

    added course name
    BME 2315

--- python_practice.py
+++ python_practice.py
@@ -7,6 +7,7 @@
 
 #name: Mona Abou-Assi 
 #year: 2
+#class: BME 2315
 
 # Write pseudocode that will input a integer N and output the sum of the first N numbers in the fibonacci sequence.
 # Fibonacci sequence starts: 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
